c99e2b595c375d3db51b07b85147a082e5dbde24
paritoshparashar <papa00001@stud.uni-saarland.de>
2024-05-20 17:10:52 +0200

    Connected till scaling in the main file

--- src/main.c
+++ src/main.c
@@ -24,8 +24,18 @@ int main(int const argc, char **const argv) {
      * Hint: The width and height of the image have to be accessible in the
      * scope of this function.
      */
-    // TODO: Implement me!
 
+       int* width;
+       int* height;
+
+       float* img_pointer = read_image_from_file (image_file_name , width , height);
+
+       if (img_pointer == NULL)
+       {
+              return 1;
+       }
+       
+       
     /**
      * Blur the image by using convolve with the given Gaussian kernel matrix
      * gaussian_k (defined in gaussian_kernel.h). The width of the matrix is
@@ -33,7 +43,15 @@ int main(int const argc, char **const argv) {
      *
      * Afterwards, write the resulting blurred image to the file out_blur.pgm.
      */
-    // TODO: Implement me!
+
+       float* convolved_img = array_init ( (*width) * (*height) );
+
+       convolve ( convolved_img , img_pointer , *width , *height , gaussian_k , gaussian_w , gaussian_h);
+       array_destroy (img_pointer);
+
+       write_image_to_file (convolved_img , *width , *height , "out_blur.pgm");
+       array_destroy (convolved_img);
+
 
     /**
      * Compute the derivation of the blurred image computed above in both x and
@@ -42,7 +60,41 @@ int main(int const argc, char **const argv) {
      * Afterwards, rescale both results and write them to the files out_d_x.pgm
      * and out_d_y.pgm respectively.
      */
-    // TODO: Implement me!
+
+       float* blur_img_pointer = read_image_from_file ("out_blur.pgm", width , height);
+
+
+       // Compute derivation in 'x' and 'y' direction
+
+       float* x_derived_img = array_init ((*width) * (*height));
+       derivation_x_direction ( x_derived_img , blur_img_pointer , *width , *height);
+
+       float* y_derived_img = array_init ((*width) * (*height));
+       derivation_x_direction ( y_derived_img , blur_img_pointer , *width , *height);
+
+       array_destroy (blur_img_pointer);
+
+
+       // Rescale both 'x' and 'y' derived result
+
+       float* x_scaled_img = array_init ((*width) * (*height));
+       scale_image ( x_scaled_img , x_derived_img , *width , *height);
+
+       float* y_scaled_img = array_init ((*width) * (*height));
+       scale_image ( y_scaled_img , y_derived_img , *width , *height);
+
+       array_destroy (x_derived_img);
+       array_destroy (y_derived_img);
+
+
+       // Write the scaled images to respective pgm files
+
+       write_image_to_file ( x_scaled_img , *width , *height , "out_d_x.pgm");
+       write_image_to_file ( y_scaled_img , *width , *height , "out_d_y.pgm");
+
+       array_destroy (x_scaled_img);
+       array_destroy (y_scaled_img);
+
 
     /**
      * Compute the gradient magnitude of the blurred image by using the
